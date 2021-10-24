c20df3bea49b04d36573b902e4da02a012f13e65
Christos Kolonas <ckolonas@csd.auth.gr>
2021-10-25 02:42:11 +0300

    fixed search query

--- genius.js
+++ genius.js
@@ -17,20 +17,26 @@ server.listen(PORT,()=>{
     console.log('Server running at PORT:'+PORT);
 })
 
-const get_data = async()=>{
-  const response =await fetch("https://genius.com/Thouxanbanfauni-american-muscle-lyrics");
+const get_data = async(url)=>{
+  const response =await fetch(url);
   const data = response.text()
   return data;
 }
+
+const get_search_results = async (query)=>{
+  const response = await fetch("https://api.genius.com/search?q="+encodeURI(query.trim())+"&access_token=oevAmuh71xZFg1T9WhQfdPPXl4WcbIg1zW52b-NSMF68dO1rliZx8zRdXZUK1yxQ").then((res)=>{console.log(res.json())})
+  //return response.json()
+}
+get_search_results("way 2 sexy")
 
 const scraper = (data)=>{
   const $ =  cheerio.load(data);
   const lyrics = $("p").text()
   return lyrics;
 }
-const do_that = async ()=>{
-  const data = await get_data();
-  console.log(scraper(data))
+const do_that = async (url)=>{
+  const data = await get_data(url);
+  return scraper(data);
 }
 
 
@@ -45,11 +51,17 @@ const main_func = async ()=>{
             'artist' :artist
         })
     })
-    console.log(tracks_json)
+    for (t in tracks_json){
+      result = await get_search_results(tracks_json[t].song+" "+tracks_json[t].artist)
+
+      //lyrics = do_that(url)
+    }
+    let search_results = await get_search_results()
+
 }
 
 
 
-main_func()
+//main_func()
 
 //do_that()
