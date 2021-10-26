a59c3b1e5c5a23c73bb3e00d1f438538691f6c52
Christos Kolonas <ckolonas@csd.auth.gr>
2021-10-26 16:00:44 +0300

    more error handling

--- genius.js
+++ genius.js
@@ -1,110 +1,118 @@
 import fetch from 'node-fetch';
 import http from 'http'
 import cheerio from 'cheerio';
-import fs, { existsSync } from 'fs';
+import fs, {
+  existsSync
+} from 'fs';
 
 const PORT = 3000;
 const token = "oevAmuh71xZFg1T9WhQfdPPXl4WcbIg1zW52b-NSMF68dO1rliZx8zRdXZUK1yxQ";
 const exportFile = "exported.json"
 
-const readSongFile= (filePath)=>{
-    fs.stat(filePath,(err,stat)=>{
-      if(err && err.code === 'ENOENT'){
-        throw ('Could not find songs .txt file.')
-      }
-    })
-    const storedtext = fs.readFileSync(filePath).toString();
-      if(storedtext.length==0){
-        throw ('Songs .txt is empty')
-      }  
- 
+const readSongFile = (filePath) => {
+  fs.stat(filePath, (err, stat) => {
+    if (err && err.code === 'ENOENT') {
+      throw ('Could not find songs .txt file.')
+    }
+  })
+  const storedtext = fs.readFileSync(filePath).toString();
+  if (storedtext.length == 0) {
+    throw ('Songs .txt is empty')
+  }
+
   return storedtext
 }
 
-const server = http.createServer((req,res)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    res.end('Hello World');
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
 
 });
 
-server.listen(PORT,()=>{
-    console.log('Server running at PORT:'+PORT);
+server.listen(PORT, () => {
+  console.log('Server running at PORT:' + PORT);
 })
 
-const get_data = async(url)=>{
-  const response =await fetch(url);
+const get_data = async (url) => {
+  const response = await fetch(url);
   const data = response.text()
   return data;
 }
 
-const get_results = async (query)=>{
+const get_results = async (query) => {
   let rj = null
   const uri = encodeURI(query.trim())
-  await fetch("https://api.genius.com/search?q="+ uri +"&access_token="+token)
-  .then((res)=>res.json().then((rjson)=>{
-    rj = rjson
-  }));
+  await fetch("https://api.genius.com/search?q=" + uri + "&access_token=" + token)
+    .then((res) => res.json().then((rjson) => {
+      rj = rjson
+    }));
   return rj
 }
 
-const get_hit_paths = (rj)=>{
-  return rj.response.hits.map((el)=>{
+const get_hit_paths = (rj) => {
+  return rj.response.hits.map((el) => {
     return el.result.path
   })
 }
 
-const scrapper = async (url)=>{
+const scrapper = async (url) => {
   const data = await get_data(url);
-  const $ =  cheerio.load(data);
+  const $ = cheerio.load(data);
   const lyrics = $("p").text()
   return new Object({
-    'url':url,
-    'lyrics':lyrics
+    'url': url,
+    'lyrics': lyrics
   });
 }
 
-const exportToJSON = (jsonFile) =>{
-  try{
-    if(!existsSync(exportFile))
+const exportToJSON = (jsonFile) => {
+  try {
+    if (!existsSync(exportFile))
       throw new Error('Did not find export File. Creating one now.')
-  }catch(err){
-      console.log(err)
+  } catch (err) {
+    console.log(err)
   }
-  try{
-    fs.writeFileSync(exportFile,"","utf-8")
-  }catch(err){
+  try {
+    fs.writeFileSync(exportFile, "", "utf-8")
+  } catch (err) {
     console.log(err)
   }
 
-  fs.appendFileSync(exportFile,JSON.stringify(jsonFile),"utf-8")
+  fs.appendFileSync(exportFile, JSON.stringify(jsonFile), "utf-8")
 }
 
-const initiateTask = async ()=>{
+const initiateTask = async () => {
+
+  try {
     let tracks = readSongFile('songs.txt').split("\n");
     let scrappedLyrics = {
-      'songs':[]
+      'songs': []
     }
 
-    let tracks_json = tracks.map((track)=>{
-        let [song,artist] = track.split(",")
-        return new Object({
-            'song': song,
-            'artist' :artist
-        })
+    let tracks_json = tracks.map((track) => {
+      let [song, artist] = track.split(",")
+      return new Object({
+        'song': song,
+        'artist': artist
+      })
     })
-    for (let t in tracks_json){
-      let hits = await get_results(tracks_json[t].song+" "+tracks_json[t].artist);
+    for (let t in tracks_json) {
+      let hits = await get_results(tracks_json[t].song + " " + tracks_json[t].artist);
       let paths = get_hit_paths(hits);
-      for(let p in paths){
-        let song_url = "http://genius.com"+paths[p];
+      for (let p in paths) {
+        let song_url = "http://genius.com" + paths[p];
         const lyrics = await scrapper(song_url)
         scrappedLyrics.songs.push(lyrics)
       }
     }
-
     exportToJSON(scrappedLyrics);
     console.log('Task Finished');
+  } catch (err) {
+    console.log('Could not complete task. Reason: ' + err)
+  }
+
+
 }
 
 
